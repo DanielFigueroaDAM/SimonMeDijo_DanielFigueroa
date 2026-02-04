9fae229a44636bfdd7b5c142d2c03a4c200b9b2e
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2026-02-04 12:04:07 +0100

    terminado controllerSQLITE

--- app/src/main/java/com/dam/simonmedijo/MyVM.kt
+++ app/src/main/java/com/dam/simonmedijo/MyVM.kt
@@ -1,6 +1,7 @@
 package com.dam.simonmedijo
 
 import android.app.Application
+import android.service.controls.Control
 import android.util.Log
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.AndroidViewModel
@@ -119,10 +120,10 @@ class MyVM(application: Application) : AndroidViewModel(application){
      * @author Daniel Figueroa Vidal
      */
     fun comprobarRecord(){
-        if(Datos.ronda.value > ControllerSQLite.obtenerRecord(getApplication()).record) { // Se llama al controller para obtener el record
-            record.value = Datos.ronda.value
-            ControllerSQLite.actualizarRecord(Datos.ronda.value, Date(), getApplication()) // Se actualiza en el caso de que sea necesario
-        }
+        ControllerSQLite.leerBaseDatos(getApplication())
+        record.value = Datos.ronda.value
+        ControllerSQLite.manejadorRecords(nuevoRecord=Datos.ronda.value, fecha =  Date(), context=getApplication()) // Se actualiza en el caso de que sea necesario
+
     }
 
 }
